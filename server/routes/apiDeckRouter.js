477b2e1eabddfa88ad3308d1b217e660088e98c9
Richard Boothe <richardjboothe@me.com>
2016-09-13 16:10:58 -0500

    add mocha

--- server/routes/apiDeckRouter.js
+++ server/routes/apiDeckRouter.js
@@ -3,6 +3,19 @@ let express     = require('express');
 let apiDeckRouter = express.Router();
 
 /*
+var orderedDeck = function() {
+  var suits = [ '♥', '♣', '♠', '♦' ];
+  var values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ];
+  var deck = [];
+
+  suits.forEach(function(suit) {
+    values.forEach(function(value) {
+      deck.push(value + suit);
+    });
+  });
+
+  return deck;
+};
 
 const shuffleDeck = (deck) => {
 
--- test/testShffle.js
+++
@@ -1,3 +0,0 @@
-var expect = require('chai').expect;
-
-expect(true).to.be.true;
