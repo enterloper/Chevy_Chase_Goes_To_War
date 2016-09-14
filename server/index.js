f28beb872161ba5b53292c7eace08e1b7be82fd7
Richard Boothe <richardjboothe@me.com>
2016-09-14 17:11:22 -0500

    fix flaw

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(morgan());
 
+
 //STATIC FILES SO TASTY!
 app.use( express.static(__dirname + '/../client') );
 
